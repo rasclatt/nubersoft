2bedd3b91f04142d3f32d5aaffecce8ff70095de
rasclatt <rasclatt@me.com>
2017-05-22 16:40:47 -0700

    Removed Deprecated Files
    Missed files for removal

--- plugins/plugin.DBDL/js/javascript.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	$("#dbdl-click").click(function() {
-			$(".dbdl-drop").slideToggle(200);
-		});
-	$("#save-reload").click(function() {
-			window.setTimeout(function(){location.reload()},5000);
-		});
-});
