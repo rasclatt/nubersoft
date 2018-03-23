8c39ea5ff1f838ab9eadd97078296cc77e058eb6
rasclatt <rasclatt@me.com>
2018-03-23 11:31:11 -0700

    Reset Password Revision

--- core/namespaces/nPlugins/Nubersoft/Password/Model.php
+++ core/namespaces/nPlugins/Nubersoft/Password/Model.php
@@ -4,116 +4,116 @@ namespace nPlugins\Nubersoft\Password;
 class Model extends \Nubersoft\Emailer
 	{
 		public	function sendPassword()
-			{
-				# Convert the incoming form values to array
-				$formData	=	$this->getHelper('nForm')->deliverToArray();
-				# Match the token
-				$match		=	$this->getHelper('nToken')->resetTokenOnMatch($formData,'nProcessor','page');
-				$registry	=	$this->getDefaultMessaging();
-				# If match token
-				if($match) {
-					$userId		=	$this->validUser($formData['email']);
-					//$this->ajaxResponse(array('alert'=>print_r($this->nQuery()->query("select * from users where email = :0",array($formData['email']))->getResults(),1)));
-					# If there user is valid
-					if($userId) {
-						$tempPassword	=	$this->createTempPass();
-						$emailer		=	$this->addTo($formData['email'])
-											->addFrom(WEBMASTER)
-											->addSubject($registry['email']['subject'])
-											->useHtml()
-											->addMessage(str_replace('~password~',$tempPassword,$registry['email']['message']),'default')
-											->send();
-						
-						if($emailer) {
-						
-							$this->nQuery()->query("UPDATE `users` SET `reset_password` = :0, `timestamp` = '".date('Y-m-d H:i:s')."' where `ID` = '{$userId}'",array($this->passwordHash($tempPassword),));
-						
-							if($this->isAjaxRequest()) {
-								$this->ajaxResponse(array(
-									'alert'=>$registry['browser']['success'],
-									'html'=>array(' ','<div class="nbr_success">Password Sent Successfully</div>'),
-									'sendto'=>array('#loadspot_modal','.recover_password_request_msg'),
-									'FX'=> [
-										'acton'=> [
-											'#loadspot_modal'
-										],
-										'fx' => [
-											'addClass::visible'
-										]
+		{
+			# Convert the incoming form values to array
+			$formData	=	$this->getHelper('nForm')->deliverToArray();
+			# Match the token
+			$match		=	$this->getHelper('nToken')->resetTokenOnMatch($formData,'nProcessor','page');
+			$registry	=	$this->getDefaultMessaging();
+			# If match token
+			if($match) {
+				$userId		=	$this->validUser($formData['email']);
+				//$this->ajaxResponse(array('alert'=>print_r($this->nQuery()->query("select * from users where email = :0",array($formData['email']))->getResults(),1)));
+				# If there user is valid
+				if($userId) {
+					$tempPassword	=	$this->createTempPass();
+					$emailer		=	$this->addTo($formData['email'])
+										->addFrom(WEBMASTER)
+										->addSubject($registry['email']['subject'])
+										->useHtml()
+										->addMessage(str_replace('~password~',$tempPassword,$registry['email']['message']),'default')
+										->send();
+
+					if($emailer) {
+
+						$this->nQuery()->query("UPDATE `users` SET `reset_password` = :0, `timestamp` = '".date('Y-m-d H:i:s')."' where `ID` = '{$userId}'",array($this->passwordHash($tempPassword),));
+
+						if($this->isAjaxRequest()) {
+							$this->ajaxResponse(array(
+								'alert'=>$registry['browser']['success'],
+								'html'=>array(' ','<div class="nbr_success">Password Sent Successfully</div>'),
+								'sendto'=>array('#loadspot_modal','.recover_password_request_msg'),
+								'FX'=> [
+									'acton'=> [
+										'#loadspot_modal'
+									],
+									'fx' => [
+										'removeClass'
 									]
-								));
-							}
-							else {
-								$this->saveSetting('event',array('recover_password_request'=>true));
-							}
+								]
+							));
 						}
 						else {
-							if($this->isAjaxRequest()) {
-								die(json_encode(array(
-									'alert'=>$registry['browser']['error'],
-									'html'=>array(' '),
-									'sendto'=>array('#loadspot_modal'),
-									'FX'=> [
-										'acton'=> [
-											'#loadspot_modal'
-										],
-										'fx' => [
-											'addClass::visible'
-										]
-									]
-								)));
-							}
-							else {
-								$this->saveSetting('event',array('recover_password_request'=>$registry['browser']['error']));
-								$this->saveError('event',array('recover_password_request'=>false));
-							}
+							$this->saveSetting('event',array('recover_password_request'=>true));
 						}
 					}
 					else {
 						if($this->isAjaxRequest()) {
 							die(json_encode(array(
-								'alert'=>$registry['browser']['fail'],
+								'alert'=>$registry['browser']['error'],
 								'html'=>array(' '),
 								'sendto'=>array('#loadspot_modal'),
 								'FX'=> [
-										'acton'=> [
-											'#loadspot_modal'
-										],
-										'fx' => [
-											'addClass::visible'
-										]
+									'acton'=> [
+										'#loadspot_modal'
+									],
+									'fx' => [
+										'removeClass'
 									]
+								]
 							)));
 						}
 						else {
-							$this->saveSetting('event',array('recover_password_request'=>$registry['browser']['fail']));
-							$this->saveIncidental('event',array('recover_password_request'=>false));
+							$this->saveSetting('event',array('recover_password_request'=>$registry['browser']['error']));
+							$this->saveError('event',array('recover_password_request'=>false));
 						}
 					}
 				}
 				else {
 					if($this->isAjaxRequest()) {
-						$msg	=	'Form token has expired or invalid. Reload page to reset.';
 						die(json_encode(array(
-							'alert'=>$msg,
+							'alert'=>$registry['browser']['fail'],
 							'html'=>array(' '),
 							'sendto'=>array('#loadspot_modal'),
 							'FX'=> [
-										'acton'=> [
-											'#loadspot_modal'
-										],
-										'fx' => [
-											'addClass::visible'
-										]
+									'acton'=> [
+										'#loadspot_modal'
+									],
+									'fx' => [
+										'removeClass'
 									]
+								]
 						)));
 					}
 					else {
-						$this->saveSetting('event',array('recover_password_request'=>$msg));
+						$this->saveSetting('event',array('recover_password_request'=>$registry['browser']['fail']));
 						$this->saveIncidental('event',array('recover_password_request'=>false));
 					}
 				}
 			}
+			else {
+				if($this->isAjaxRequest()) {
+					$msg	=	'Form token has expired or invalid. Reload page to reset.';
+					die(json_encode(array(
+						'alert'=>$msg,
+						'html'=>array(' '),
+						'sendto'=>array('#loadspot_modal'),
+						'FX'=> [
+									'acton'=> [
+										'#loadspot_modal'
+									],
+									'fx' => [
+										'removeClass'
+									]
+								]
+					)));
+				}
+				else {
+					$this->saveSetting('event',array('recover_password_request'=>$msg));
+					$this->saveIncidental('event',array('recover_password_request'=>false));
+				}
+			}
+		}
 		
 		private	function validUser($email)
 			{
