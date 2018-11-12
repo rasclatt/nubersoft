5e381735c7a7186aef458c568b9d011757b54655
rasclatt <rasclatt@me.com>
2018-11-12 12:03:51 -0800

    Remove Client

--- client/settings/dbcreds.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DB_HOST', "bXlzcWxjbHVzdGVyNA==");
-define('DB_NAME', "bl91X2Jlcg==");
-define('DB_USER', "cmFzXzMyMV9jbGF0dDMyMQ==");
-define('DB_PASS', "cmhFXzEyM3NVXzMyMXNfMDk4");
-define('DB_CHARSET', 'utf8');
--- client/template/rasclatt/frontend/index.php
+++
@@ -1,2 +0,0 @@
-<?php
-include(__DIR__.DS.'index.'.(($this->getSession('editor'))? 'editor' : 'view').'.php');
