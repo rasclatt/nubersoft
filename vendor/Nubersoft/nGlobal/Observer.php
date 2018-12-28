4b94f99dc5151cd29898a0397547c5070b44ed37
rasclatt <rasclatt@me.com>
2018-12-28 10:00:58 -0800

    Core Updates

--- vendor/Nubersoft/nGlobal/Observer.php
+++ vendor/Nubersoft/nGlobal/Observer.php
@@ -7,6 +7,12 @@ class Observer extends \Nubersoft\nGlobal implements \Nubersoft\nObserver
 	{
 		$this->createContainer(function(\Nubersoft\DataNode $DataNode){
 			
+			if(isset($_FILES['file']['tmp_name'])) {
+				$_FILES['file']['tmp_name']	=	array_filter($_FILES['file']['tmp_name']);
+				if(empty($_FILES['file']['tmp_name']))
+					$_FILES	=	null;
+			}
+			
 			foreach([
 				'_ENV' => $_ENV,
 				'_GET' => $_GET,
