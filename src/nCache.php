618915cadecca4fb1de8ae3ef4fe0b3444be38d5
rasclatt <rasclatt@me.com>
2020-04-21 23:11:50 -0700

    Update Cacher

--- src/nCache.php
+++ src/nCache.php
@@ -3,18 +3,16 @@ namespace Nubersoft;
 
 class    nCache extends \Nubersoft\nApp
 {
-    protected    $destination,
-                $has_layout,
-                $layout;
-    private        $refresh    =    false;
+    protected   $destination,$has_layout,$layout,$basepath;
+    private $refresh    =    false;
     
-    public    function delete()
+    public function delete()
     {
         $this->refresh    =    true;
         return $this;
     }
     
-    public    function start($path_to_file)
+    public function start($path_to_file)
     {
         $this->destination    =    $path_to_file;
         $this->has_layout    =    false;
@@ -34,12 +32,12 @@ class    nCache extends \Nubersoft\nApp
         return $this;
     }
     
-    public    function isCached()
+    public function isCached()
     {
         return $this->has_layout;
     }
     
-    public    function render()
+    public function render()
     {
         if($this->isCached()){
             include($this->destination);
@@ -48,9 +46,36 @@ class    nCache extends \Nubersoft\nApp
         ob_end_clean();
         
         if(!$this->isCached()) {
-            file_put_contents($this->destination, $this->layout);
+            $path   =   pathinfo($this->destination, PATHINFO_DIRNAME);
+            if($this->isDir($path, 1))
+                file_put_contents($this->destination, $this->layout);
         }
         
         return $this->layout;
     }
+	/**
+	 *	@description	
+	 */
+	public function setBasePath($path)
+	{
+        $this->basepath =   $path;
+        return $this;
+	}
+	/**
+	 *	@description	
+	 */
+	public function __($func, $def = 'index.html')
+	{
+        if(empty($this->basepath))
+            $path =   NBR_CLIENT_CACHE.DS.'pages'.DS.$this->getDataNode('routing')['ID'].DS.$def;
+        else
+            $path   =   $this->basepath.$def;
+        
+        $this->start($path);
+        if(!$this->isCached()) {
+            echo (is_callable($func))? $this->getHelper('nReflect')->reflectFunction($func) : $func;
+        }
+        
+        return $this->render();
+	}
 }
